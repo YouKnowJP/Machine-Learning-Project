5e06098f112d64e970c05435db2271662d000f1d
youknowjp <thongphambusiness@gmail.com>
2025-02-16 02:36:27 -0700

    Rename Directory

--- CNN-From-Scratch/loss/categorical_cross_entropy.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# Define loss function
-def categorical_cross_entropy(y_true, y_pred, epsilon=1e-12):
-    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
-    return -np.sum(y_true * np.log(y_pred))
-
-def categorical_cross_entropy_prime(y_true, y_pred):
-    return y_pred - y_true
